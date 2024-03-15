2f1e6ee55c5e8065883b27d3f8b3ff6d35694a77
Théo Duflos <theo.duflos.yokoko@gmail.com>
2024-03-15 11:47:26 +0100

    feat: :sparkles: Add content in the merise page and start dark mode button

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 function openMenu() {
-    var x = document.getElementById("links");
+    const x = document.getElementById("links");
     if (x.style.display === "block") {
-      x.style.display = "none";
+        x.style.display = "none";
     } else {
-      x.style.display = "block";
+        x.style.display = "block";
     }
-  }
+}
+
+document.querySelector(".btToggle").addEventListener("click", changeThemeMode);
+
+function changeThemeMode() {
+    console.log("test");
+    const x = document.body.querySelector(".container");
+    console.log(x);
+    x.classList.toggle("darkmode");
+}
