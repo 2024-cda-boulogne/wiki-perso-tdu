51f1b895bbc049f5eb7062cace76d02a5e232243
Théo Duflos <theo.duflos.yokoko@gmail.com>
2024-03-18 16:42:03 +0100

    fix: :bug: Fix broken link in navigation menu

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-document
-    .querySelector("#burgermenu")
-    .addEventListener("click", openMenu);
+document.querySelector("#burgermenu").addEventListener("click", openMenu);
 
 function openMenu() {
     const x = document.getElementById("links");
@@ -17,6 +15,8 @@ document
 
 function changeThemeMode() {
     const x = document.body;
+    const y = document.querySelector(".theme-toggle");
+    y.classList.toggle("darkmode");
     x.classList.toggle("darkmode");
     if (x.classList.contains("darkmode")) {
         localStorage.setItem("theme-mode", "dark");
@@ -29,22 +29,23 @@ window.addEventListener("load", setThemeByLocalStorage, false);
 
 function setThemeByLocalStorage() {
     const x = document.body;
+    const y = document.querySelector(".theme-toggle");
     if (localStorage.getItem("theme-mode") === "dark") {
         x.classList.add("darkmode");
+        y.classList.add("darkmode");
     } else {
+        y.classList.remove("darkmode");
         x.classList.remove("darkmode");
     }
 }
 
-
-const showNumber = document.getElementById('about');
-const favDialog = document.getElementById('favDialog');
-const btnModal = document.querySelector('.btn-modal');
-showNumber.addEventListener('click', () => {
-  favDialog.showModal();
+const showNumber = document.getElementById("about");
+const favDialog = document.getElementById("favDialog");
+const btnModal = document.querySelector(".btn-modal");
+showNumber.addEventListener("click", () => {
+    favDialog.showModal();
 });
 
-btnModal.addEventListener('click', () => {
-  favDialog.close();
+btnModal.addEventListener("click", () => {
+    favDialog.close();
 });
-
